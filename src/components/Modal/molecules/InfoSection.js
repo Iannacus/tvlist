d80870441af868b5ba5c71f99167bd55f67bf1f5
Ian Rosas <ian.rosasmal@gmail.com>
2021-11-12 11:42:13 -0600

    change description text when is a null value

--- src/components/Modal/molecules/InfoSection.js
+++ src/components/Modal/molecules/InfoSection.js
@@ -4,12 +4,15 @@ import StarsRate from './stars-rate/StarsRate';
 import Resume from '../atoms/Paragraph/Resume';
 
 const InfoSection = ({title, description, calification, links}) => {
+  console.log(description);
   return (
     <div className="info-container">
       <ModalTitle links={links}>{title}</ModalTitle>
       <StarsRate>{calification}</StarsRate>
       <Resume>
-        {description}
+        {description !== null ?
+          description : '<p>The tv series description was not founded</p>'
+        }
       </Resume>
     </div>
   )
